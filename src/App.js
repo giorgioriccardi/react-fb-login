79eb3cfe70cfd88ca28394439ce546ca89a80951
giorgio25b <giorgio25b@gmail.com>
2018-05-25 15:26:04 -0700

    Add initial state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// Import custom component
+import Facebook_login from './components/Facebook_login';
 
 class App extends Component {
   render() {
@@ -8,11 +10,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">A React FB Login Component</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Login with FB.
         </p>
+        {/* Render custom component */}
+        <Facebook_login />
       </div>
     );
   }
---
+++ src/components/Facebook_login.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+import FacebookLogin from "react-facebook-login";
+
+export default class Facebook_login extends Component {
+  state = {
+    isLoggedIn: false,
+    userID: "",
+    name: "",
+    email: "",
+    picture: ""
+  };
+
+  render() {
+    return (
+      <div>
+        <p>Render component logic</p>
+      </div>
+    )
+  }
+}
